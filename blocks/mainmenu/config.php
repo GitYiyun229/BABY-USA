db088c25471a7fae325a35b5faae0390b0b74fa9
hoangthong2950 <hoangthong2950@gmail.com>
2024-04-02 16:53:47 +0700

    sua header menu home

--- blocks/mainmenu/config.php
+++ blocks/mainmenu/config.php
@@ -1,37 +1,54 @@
 <?php 
 	$params = array (
 		'suffix' => array(
-					'name' => 'Hậu tố',
+					'name' => FSText::_('Hậu tố'),
 					'type' => 'text',
-					'default' => '_menu'
+					'default' => '_mainmenu'
 					),
-		'id' => array(
-					'name' => 'Id (dùng cho qcáo bên ngoài trang)',
+		'limit' => array(
+					'name' => FSText::_('Giới hạn'),
 					'type' => 'text',
-					'default' => 'divAdLeft',
-					'comment' => 'divAdLeft dùng cho bên trái, divAdRight cho bên phải'
+					'default' => '6'
 					),
+			
 		'style' => array(
-					'name'=>'Style',
+					'name'=> FSText::_('Kiểu Style'),
 					'type' => 'select',
-					'value' => array('default' => 'Mặc định',
-									'bottommenu'=>'Phía dưới',
-									'service'=>'Dịch vụ',
-									'static'=>'Trang tĩnh',
-									)
-			),
-		'category_id' => array(
-					'name'=>'Nhóm menu',
+					'value' => array(
+                                    'default' => FSText::_('Mặc định'),
+                                    'link_menu'=> 'Menu Liên kết',
+                                    'default_news' => FSText::_('Tư vấn tuyển sinh'),
+                                    'bottom' => FSText::_('Menu bottom dưới footer'),
+                                    'responsive'=> FSText::_('Menu mặc định responsive'),
+                                    //'contents'=> FSText::_('Menu giới thiệu click'),
+                                    //'contents2'=> FSText::_('Menu giới thiệu show'),
+                                    //'main_activities'=> FSText::_('Các hoạt động chính')
+                                    )
+		),
+        'group' => array(
+					'name'=>FSText::_('Nhóm menu'),
 					'type' => 'select',
 					'value' => get_category(),
+					//'attr' => array('multiple' => 'multiple'),
+		 ),
+         
+         'contents_id' => array(
+					'name'=>'Bài viết',
+					'type' => 'select',
+					'value' => get_contents(),
 					'attr' => array('multiple' => 'multiple'),
-			),
+		),   
+
 	);
-	function get_category(){
-		global $db;
-			$query = " SELECT group_name, id 
-						FROM fs_menus_groups 
-						";
+    
+    function get_category(){
+            $fstable  = FSFactory:: getClass('fstable');
+			$table_name = 'fs_menus_groups';
+            
+		    global $db;
+			$query = ' SELECT group_name, id
+						FROM '.$table_name.'
+						WHERE published = 1 ';
 			$sql = $db->query($query);
 			$result = $db->getObjectList();
 			if(!$result)
@@ -42,5 +59,23 @@
             }
 			return $arr_group;
 	}
-	
+    
+    function get_contents(){
+            $fstable  = FSFactory:: getClass('fstable');
+			$table_name = $fstable->_('fs_contents');
+            
+		    global $db;
+			$query = ' SELECT title, id 
+						FROM '.$table_name.' 
+						';
+			$sql = $db->query($query);
+			$result = $db->getObjectList();
+			if(!$result)
+			     return;
+			$arr_group = array();
+            foreach($result as $item){
+            	$arr_group[$item -> id] = $item -> title;
+            }
+			return $arr_group;
+	}
 ?>
--- blocks/mainmenu/controllers/mainmenu.php
+++ blocks/mainmenu/controllers/mainmenu.php
@@ -1,219 +1,297 @@
 <?php
+/*
+ * Huy write
+ */
+	// models
+	include 'blocks/mainmenu/models/mainmenu.php';
 
-include 'blocks/mainmenu/models/mainmenu.php';
-
-class MainMenuBControllersMainMenu
-{
-	function display($parameters, $title)
+	class MainMenuBControllersMainMenu
 	{
-		$group = $parameters->getParams('group');
-		$style = $parameters->getParams('style');
+		function __construct()
+		{
+		}
+		function display($parameters,$title)
+		{
+			$group = $parameters->getParams('group');
+		
+			$style = $parameters->getParams('style');
+			//$limit = $parameters->getParams('limit');
+            $contents_id = $parameters->getParams('contents_id');
+			$style = $style?$style:'default';
+			// $group = isset($parameters['group']) ? $parameters['group'] : '1';
+//			$style = isset($parameters['style']) ? $parameters['style'] : 'default';
+			if(!$group)
+				return;
+			// call models
+			$model = new MainMenuBModelsMainMenu();
+            $id = FSInput::get('id',0,'int');
+            $module = FSInput::get('module');
 
-		if (!$group) {
-			$group = $parameters->getParams('category_id');
+            if($module == 'contents' && $contents_id && $id){
+                if(strpos(",$contents_id","$id") !== false ){
+                    $list = $model->getList($group);
+                }else{
+                    return;
+                }
+            }else{
+                $list = $model->getList($group);
+            }
+			if(!$list)
+				return;
+			// var_dump($list);die;
+			$arr_activated = array();
+			$children = array();
+			if(!count($list))
+				return;
+			if($style == 'megamenu' || $style == 'megamenu_moblie' || $style == 'icon_and_popup'){
+				$level_0 = array();
+				$children = array();
+				$arr_activated = array();
+				foreach ($list as $item) {
+					$arr_activated[$item->id] = 0;
+					if(!$item -> parent_id){
+						$level_0[] = $item;
+					}else{
+						if(!isset($children[$item -> parent_id]))
+							$children[$item -> parent_id] = array();
+						$children[$item -> parent_id][] = $item;
+					}
+
+					// check ativated
+					$activated  = $this -> check_active($item -> link);
+					if($activated){
+						$arr_activated[$item->id] = 1;
+						if(isset($item -> parent_id) && !empty($item -> parent_id) )
+							$arr_activated[$item -> parent_id] = 1;
+					}
+				}
+//				var_dump($children);
+			}
+//die;
+	 		foreach($list as $item){
+				$arr_activated[$item->id] = 0;
+				// check ativated
+				$activated  = $this -> check_activated($item -> link);
+				if($activated){
+					$arr_activated[$item->id] = 1;
+					if(isset($item -> parent_id) && !empty($item -> parent_id) )
+						$arr_activated[$item -> parent_id] = 1;
+				}
+			}
+
+			// call views
+			include 'blocks/mainmenu/views/mainmenu/'.$style.'.php';
 		}
 
-		$style = $style ? $style : 'default';
-		 
-		if (!$group)
-			return;
-		 
-		$model = new MainMenuBModelsMainMenu();
-		$list = $model->getList($group);
-		if (!$list)
-			return;
-
-		// call views
-		include 'blocks/mainmenu/views/mainmenu/' . $style . '.php';
-	}
-
-	function get_params($url)
-	{
-		$url_reduced  = substr($url, 10); // width : index.php
-		$array_buffer = explode('&', $url_reduced, 10);
-		$array_params = array();
-		for ($i  = 0; $i < count($array_buffer); $i++) {
-			$item = $array_buffer[$i];
-			$pos_sepa = strpos($item, '=');
-			$array_params[substr($item, 0, $pos_sepa)] = substr($item, $pos_sepa + 1);
+	/*
+		 * get Array params
+		 */
+		function get_params($url){
+			$url_reduced  = substr($url,10); // width : index.php
+			$array_buffer = explode('&',$url_reduced,10);
+			$array_params = array();
+			for($i  = 0; $i < count($array_buffer) ; $i ++ ){
+				$item = $array_buffer[$i];
+				$pos_sepa = strpos($item,'=');
+				$array_params[substr($item,0,$pos_sepa)] = substr($item,$pos_sepa+1);
+			}
+			return $array_params;
 		}
-		return $array_params;
-	}
+		function check_active($link=''){
+			$link_rewrite = FSRoute::_($link)."--";
+			$url_current = URL_ROOT.substr($_SERVER['REQUEST_URI'],1);
 
-	function check_active($link = '')
-	{
-		$link_rewrite = FSRoute::_($link) . "--";
-		$url_current = URL_ROOT . substr($_SERVER['REQUEST_URI'], 1);
-
-		if ($link_rewrite == $url_current)
-			return true;
-		$module = FSInput::get('module');
-		$view = FSInput::get('view');
-		if ($module == 'news' && ($view == 'news' || $view == 'cat')) {
-			$ccode = FSInput::get('ccode');
-			if (strpos($link, '&ccode=' . $ccode) !== false) {
+			if($link_rewrite == $url_current)
 				return true;
+			$module = FSInput::get('module');
+			$view = FSInput::get('view');
+			if($module == 'news' && ($view=='news' || $view == 'cat')){
+				$ccode = FSInput::get('ccode');
+				if(strpos($link,'&ccode='.$ccode ) != false){
+					return true;
+				}
+			}
+			return false;
+		}
+		function check_activated($url){
+			if(!$url)
+				return false;
+			$array_params  = $this ->  get_params($url);
+			$module  = isset($array_params['module'])?$array_params['module']: '';
+			$module_c = FSInput::get('module');
+			if($module != $module_c)
+				return false;
+			switch ($module){
+				case 'contact':
+				case 'admissions':
+				case 'students':
+				case 'libraries':
+				case 'partners':
+                case 'estores':
+                case 'users':
+                case 'image':
+				case 'ranks':
+				case 'course':
+					if($module == $module_c)
+						return true;
+					return false;
+				case 'news':
+					$view  = isset($array_params['view'])?$array_params['view']: $module;
+					$view_c = FSInput::get('view');
+					switch ($view){
+						case 'news':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['code'])?$array_params['code']:'';
+							$code_c = FSInput::get('code');
+							if($code == $code_c)
+								return true;
+							return false;
+						case 'cat':
+							$ccode  = isset($array_params['ccode'])?$array_params['ccode']:'';
+							$ccode_c = FSInput::get('ccode');
+							if(!empty($ccode) && $ccode_c == $ccode )
+								return true;
+							return false;
+						case 'home':
+							return true;
+						default:
+							return $view ==  $view_c ? true:false;
+					}
+                case 'event':
+					$view  = isset($array_params['view'])?$array_params['view']: $module;
+					$view_c = FSInput::get('view');
+					switch ($view){
+						case 'event':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['code'])?$array_params['code']:'';
+							$code_c = FSInput::get('code');
+							if($code == $code_c)
+								return true;
+							return false;
+						case 'cat':
+							$ccode  = isset($array_params['ccode'])?$array_params['ccode']:'';
+							$ccode_c = FSInput::get('ccode');
+							if(!empty($ccode) && $ccode_c == $ccode )
+								return true;
+							return false;
+						case 'home':
+							return true;
+						default:
+							return $view ==  $view_c ? true:false;
+					}
+                case 'gallery':
+					$view  = isset($array_params['view'])?$array_params['view']: $module;
+					$view_c = FSInput::get('view');
+					switch ($view){
+						case 'gallery':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['code'])?$array_params['code']:'';
+							$code_c = FSInput::get('code');
+							if($code == $code_c)
+								return true;
+							return false;
+						case 'cat':
+							$ccode  = isset($array_params['ccode'])?$array_params['ccode']:'';
+							$ccode_c = FSInput::get('ccode');
+							if(!empty($ccode) && $ccode_c == $ccode )
+								return true;
+							return false;
+						case 'home':
+							return true;
+						default:
+							return $view ==  $view_c ? true:false;
+					}
+
+                case 'recruitment':
+					$view  = isset($array_params['view'])?$array_params['view']: $module;
+					$view_c = FSInput::get('view');
+					switch ($view){
+						case 'recruitment':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['code'])?$array_params['code']:'';
+							$code_c = FSInput::get('code');
+							if($code == $code_c)
+								return true;
+							return false;
+						case 'cat':
+							$ccode  = isset($array_params['ccode'])?$array_params['ccode']:'';
+							$ccode_c = FSInput::get('ccode');
+							if(!empty($ccode) && $ccode_c == $ccode )
+								return true;
+							return false;
+						case 'home':
+							return true;
+						default:
+							return $view ==  $view_c ? true:false;
+					}
+				case 'contents':
+					$view  = isset($array_params['view'])?$array_params['view']: $module;
+					$view_c = FSInput::get('view');
+					switch ($view){
+						case 'content':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['code'])?$array_params['code']:'';
+							$code_c = FSInput::get('code');
+							if($code == $code_c)
+								return true;
+							return false;
+						case 'cat':
+							$ccode  = isset($array_params['ccode'])?$array_params['ccode']:'';
+							$ccode_c = FSInput::get('ccode');
+							if(!empty($ccode) && $ccode_c == $ccode )
+								return true;
+							return false;
+						case 'home':
+							return true;
+						default:
+							return $view ==  $view_c ? true:false;
+					}
+
+
+				case 'products':
+					$view  = isset($array_params['view'])?$array_params['view']: $module;
+					$view_c = FSInput::get('view');
+					switch ($view){
+						case 'product':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['code'])?$array_params['code']:'';
+							$code_c = FSInput::get('code');
+							if($code == $code_c)
+								return true;
+							return false;
+
+                        case 'service':
+							if($view != $view_c)
+								return false;
+							$code  = isset($array_params['type'])?$array_params['type']:'';
+							$code_c = FSInput::get('type');
+							if($code == $code_c)
+								return true;
+							return false;
+
+						case 'cat':
+							$ccode  = isset($array_params['ccode'])?$array_params['ccode']:'';
+							$ccode_c = FSInput::get('ccode');
+							if(!empty($ccode) && $ccode_c == $ccode )
+								return true;
+							return false;
+
+						case 'home':
+							return true;
+						default:
+							return $view ==  $view_c ? true:false;
+					}
+
+
+				return false;
 			}
 		}
-		return false;
 	}
 
-	function check_activated($url)
-	{
-		if (!$url)
-			return false;
-		$array_params  = $this->get_params($url);
-		$module  = isset($array_params['module']) ? $array_params['module'] : '';
-		$module_c = FSInput::get('module');
-		if ($module != $module_c)
-			return false;
-		switch ($module) {
-			case 'poll':
-			case 'contact':
-			case 'goals':
-			case 'gallery':
-			case 'partners':
-			case 'ranks':
-				if ($module == $module_c)
-					return true;
-				return false;
-			case 'news':
-				$view  = isset($array_params['view']) ? $array_params['view'] : $module;
-				$view_c = FSInput::get('view');
-				switch ($view) {
-					case 'news':
-						if ($view != $view_c)
-							return false;
-						$code  = isset($array_params['code']) ? $array_params['code'] : '';
-						$code_c = FSInput::get('code');
-						if ($code == $code_c)
-							return true;
-						return false;
-					case 'cat':
-						$ccode  = isset($array_params['ccode']) ? $array_params['ccode'] : '';
-						$ccode_c = FSInput::get('ccode');
-						if (!empty($ccode) && $ccode_c == $ccode)
-							return true;
-						return false;
-					case 'home':
-						return true;
-					default:
-						return $view ==  $view_c ? true : false;
-				}
-			case 'contents':
-				$view  = isset($array_params['view']) ? $array_params['view'] : $module;
-				$view_c = FSInput::get('view');
-				switch ($view) {
-					case 'contents':
-						if ($view != $view_c)
-							return false;
-						$code  = isset($array_params['code']) ? $array_params['code'] : '';
-						$code_c = FSInput::get('code');
-						if ($code == $code_c)
-							return true;
-						return false;
-					case 'cat':
-						$ccode  = isset($array_params['ccode']) ? $array_params['ccode'] : '';
-						$ccode_c = FSInput::get('ccode');
-						if (!empty($ccode) && $ccode_c == $ccode)
-							return true;
-						return false;
-					case 'home':
-						return true;
-					default:
-						return $view ==  $view_c ? true : false;
-				}
-			case 'services':
-				$view  = isset($array_params['view']) ? $array_params['view'] : $module;
-				$view_c = FSInput::get('view');
-				switch ($view) {
-					case 'service':
-						if ($view != $view_c)
-							return false;
-						$code  = isset($array_params['code']) ? $array_params['code'] : '';
-						$code_c = FSInput::get('code');
-						if ($code == $code_c)
-							return true;
-						return false;
-					case 'cat':
-						$ccode  = isset($array_params['ccode']) ? $array_params['ccode'] : '';
-						$ccode_c = FSInput::get('ccode');
-						if (!empty($ccode) && $ccode_c == $ccode)
-							return true;
-						return false;
-					case 'home':
-						return true;
-					default:
-						return $view ==  $view_c ? true : false;
-				}
-			case 'projects':
-				$view  = isset($array_params['view']) ? $array_params['view'] : $module;
-				$view_c = FSInput::get('view');
-				if ($view == 'industries' && $view_c == 'industries') {
-					return true;
-				} elseif (($view == 'home' && $view_c == 'cat') || ($view == 'home' && $view_c == 'project') || ($view == 'home' && $view_c == 'regions')) {
-					return true;
-				}
-				switch ($view) {
-					case 'project':
-						if ($view != $view_c)
-							return false;
-						$code  = isset($array_params['code']) ? $array_params['code'] : '';
-						$code_c = FSInput::get('code');
-						if ($code == $code_c)
-							return true;
-						return false;
-					case 'cat':
-						$ccode  = isset($array_params['ccode']) ? $array_params['ccode'] : '';
-						$ccode_c = FSInput::get('ccode');
-						if (!empty($ccode) && $ccode_c == $ccode)
-							return true;
-						return false;
-						//						case 'home':
-						//							return true;
-					default:
-						return $view ==  $view_c ? true : false;
-				}
-			case 'products':
-				$view  = isset($array_params['view']) ? $array_params['view'] : $module;
-				$view_c = FSInput::get('view');
-				switch ($view) {
-					case 'product':
-						if ($view != $view_c)
-							return false;
-						$code  = isset($array_params['code']) ? $array_params['code'] : '';
-						$code_c = FSInput::get('code');
-						if ($code == $code_c)
-							return true;
-						return false;
-
-					case 'categories':
-						$ccode  = isset($array_params['ccode']) ? $array_params['ccode'] : '';
-						$ccode_c = FSInput::get('ccode');
-						if (!empty($ccode) && $ccode_c == $ccode)
-							return true;
-						return false;
-					case 'home':
-						return true;
-					default:
-						return $view ==  $view_c ? true : false;
-				}
-				//				case 'video':
-				//					$view  = isset($array_params['view'])?$array_params['view']: $module;
-				//					$view_c = FSInput::get('view');
-				//					switch ($view){
-				//						case 'video':
-				//							if($view != $view_c)
-				//								return false;
-				//							$code  = isset($array_params['code'])?$array_params['code']:'';
-				//							$code_c = FSInput::get('code');
-				//							if($code == $code_c)
-				//								return true;
-				//							return false;
-				//						default:
-				//							return $view ==  $view_c ? true:false;
-				//					}
-				return false;
-		}
-	}
-}
+?>
